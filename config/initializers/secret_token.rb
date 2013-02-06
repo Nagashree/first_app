1b69896f343ae801b45bb03ca04b0cead4706806
Strata <jibran@jibran-HP-430-Notebook-PC.(none)>
2013-02-06 15:08:21 +0530

    initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cf01c908ae10001c49e41c6cad2917460bdbb945e3e9e8bd3d3feb54cc4fc1a5f26b2753df5a20990d7bef9315fcc7877eb664feea37a73113ae96c4f9561ba5'
